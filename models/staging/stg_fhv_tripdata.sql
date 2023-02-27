0e6e7407f79783b585d698d210583c7c783d98de
jimmy.tu@live.ca <jimmy.tu@live.ca>
2023-02-27 00:34:42 +0000

    Added fhv data

---
+++ models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,29 @@
+{{ config(materialized='view') }}
+
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
+  from {{ source('staging','fhv_tripdata') }}
+  where dispatching_base_num is not null 
+)
+select
+    -- identifiers
+    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    dispatching_base_num,
+    pickup_datetime,
+    dropoff_datetime,
+    cast(pulocationid as integer) as pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
+    SR_flag,
+    Affiliated_base_number
+    
+from tripdata
+where rn = 1
+
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
